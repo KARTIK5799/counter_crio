1e5a41d18d2429e7af19103ece4333ecff2efbbc
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-20 14:10:56 +0530

    Counter

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Counter from './components/Counter/Counter'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+   <div className='flex justify-center items-center min-h-screen'>
+      <Counter />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrease = () => {
+   if(count!=0){
+    setCount(count - 1);
+   }
+  };
+
+  return (
+    <div className="max-w-md mx-auto my-10 p-10 bg-gray-100 border rounded-md shadow-md">
+      <h1 className="text-4xl font-bold mb-4 text-center">{count}</h1>
+      <button
+        className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2"
+        onClick={handleIncrease}
+      >
+        Increase
+      </button>
+      <button
+        className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+        onClick={handleDecrease}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
+
