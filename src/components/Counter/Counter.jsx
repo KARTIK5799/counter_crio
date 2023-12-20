68c6f31782a0705845e9ed6a2849c1781208abce
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-20 14:26:52 +0530

    css

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -8,29 +8,31 @@ const Counter = () => {
   };
 
   const handleDecrease = () => {
-   if(count!=0){
-    setCount(count - 1);
-   }
+    if (count !== 0) {
+      setCount(count - 1);
+    }
   };
 
   return (
-    <div className="max-w-md mx-auto my-10 p-10 bg-gray-100 border rounded-md shadow-md">
-      <h1 className="text-4xl font-bold mb-4 text-center">Count: {count}</h1>
-      <button
-        className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2"
-        onClick={handleIncrease}
-      >
-        Increment
-      </button>
-      <button
-        className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-        onClick={handleDecrease}
-      >
-        Decrement
-      </button>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="max-w-md mx-auto p-6 bg-white border rounded-md shadow-md">
+        <h1 className="text-3xl font-bold mb-4 text-center">Counter App</h1>
+        <h2 className="text-lg  mb-4 text-center">Count: {count}</h2>
+        <button
+          className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2"
+          onClick={handleIncrease}
+        >
+          Increment
+        </button>
+        <button
+          className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+          onClick={handleDecrease}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 };
 
 export default Counter;
-
